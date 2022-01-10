b92d328b091820b8473c2a70fa0f65fa19e928fb
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-10 11:17:50 -0800

    add functions for subtraction, division, and multiplication
    Co-Authored-By: Alex McKnight <alex.mcknight11@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-function add(number1, number2) {
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+  // Function for addition
+
+  function add(number1, number2) {
   return number1 + number2;
   }
+
+  // Function for subtraction
+function subtract(number1, number2) {
+  return number1 - number2;
+  }
+
+
+
+  // Function for multiplication
+function multiply(number1, number2) {
+  return number1 * number2;
+  }
+
+
   
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+  //Function for division
+function divide(number1, number2) {
+  return number1 / number2;
+  } 
+
+  alert(subtract(number1, number2));
