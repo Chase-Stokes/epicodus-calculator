4dfaca535790a4dd31c80aedb7c729f98cc0de01
Chase Stokes <chasehunterstokes@gmail.com>
2022-01-12 10:53:09 -0800

    add bootstrap and jquery and modify functions to not use alerts and prompts
    Co-authored-by: Ryan Spencer <ryan.spencer1220@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+
 //   // Function for addition
 
   function add(number1, number2) {
@@ -25,19 +24,50 @@ function divide(number1, number2) {
   return number1 / number2;
   } 
 
-  alert(subtract(number1, number2));
+  //User Interface Logic
+
+  $(document).ready(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = add(number1, number2);
+      $(".output").text(result);
+    });
+    $("form#subtract").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#subtract1").val());
+      const number2 = parseInt($("#subtract2").val());
+      const result = subtract(number1, number2);
+      $(".output1").text(result);
+    });
+    $("form#multiply").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#multiply1").val());
+      const number2 = parseInt($("#multiply2").val());
+      const result = multiply(number1, number2);
+      $(".output2").text(result);
+    });
+    $("form#divide").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#divide1").val());
+      const number2 = parseInt($("#divide2").val());
+      const result = divide(number1, number2);
+      $(".output3").text(result);
+    });
+  });
 
 
   //Additional Practice -- Temperature Conversion
 
-const c = parseInt(prompt("Enter a temp in Celcius")); 
-const f = parseInt(prompt("Enter a temp in Farenheit")) ;
-  function farenToCel(f){
-    return "Temp in celcius is " +((f - 32) * (5/9));
-  }
+// const c = parseInt(prompt("Enter a temp in Celcius")); 
+// const f = parseInt(prompt("Enter a temp in Farenheit")) ;
+//   function farenToCel(f){
+//     return "Temp in celcius is " +((f - 32) * (5/9));
+//   }
 
-  function celToFaren(c){
-    return "Temp in farenheit is " +((c * 9/5) + 32);
-  }
-  alert(farenToCel(f))
-  alert(celToFaren(c))
+//   function celToFaren(c){
+//     return "Temp in farenheit is " +((c * 9/5) + 32);
+//   }
+//   alert(farenToCel(f))
+//   alert(celToFaren(c))
